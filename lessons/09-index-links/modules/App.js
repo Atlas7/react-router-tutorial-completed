da0ada8470532804050a324464f4d3ee28917ed1
atlas7 <johnnychan0302@gmail.com>
2016-06-26 16:15:58 +0100

    update App

--- lessons/09-index-links/modules/App.js
+++ lessons/09-index-links/modules/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IndexLink } from 'react-router'
+// import { IndexLink } from 'react-router'
 import NavLink from './NavLink'
 
 export default React.createClass({
